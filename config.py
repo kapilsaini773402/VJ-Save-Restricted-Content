734cfb454c6f58531720af334e2cfd1001475a0d
kapilsaini773402 <kapilsaini773402@gmail.com>
2024-09-19 15:35:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "21761132"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "595b707f4683b3e7034a6a6fc3fe5eb2")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "1BVtsOI4BuwYwyBBb9B8_21eOysfD8bs96bRegFttCwApREHbz0FanJXY_8tCpX8aYyRe_jL7C4xD05HEi8SrfmaINIyaecd6jcA8AJZqaiLOlKCIRx7mAL886eG-6BEF-U3d977Lfr35e72SGeAoTXCF9aqTGrrxzZ8cUnHy01mK_2XusfDFHQkmwFjNfLlS-SyKczSb0e-lO-z25DxG-WuSs2fbRV2sPYCb39RsF2JQOVS-qidUJBDZF4YU9LqwHS_KxmZ2q3yPpiDgDyIq3N3Hitjhnku8GEQoppYi_aURhtdCtYAOEQm3FVjtX89CoCbVYb1_PCnMfvZ4SPA3l_04mB2lZuw=")
